f071c0c21e1451983c2e3b5e077718a35b0738fc
CodeGuy <160191085+CodeGuy11@users.noreply.github.com>
2025-04-17 06:44:59 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ let cursorPrice = 15
 let clicksPerSecond = 0
 let grandmas = 0
 let grandmaPrice = 100
+let farms = 0
+let farmPrice = 1100
 function updateDisplay() {
-    document.getElementById("cookieCount").textContent = "You have " + Math.floor(cookies) + " cookies.";
-    document.getElementById("cursorCount").textContent = "You have " + cursors + " cursors.";
+    document.getElementById("cookieCount").textContent = Math.floor(cookies) + " cookies";
+    document.getElementById("cursorCount").textContent = cursors;
     document.getElementById("cursorPrice").textContent = cursorPrice;
-    document.getElementById("clicksPerSecond").textContent = clicksPerSecond + " clicks per second."
-    document.getElementById("grandmaCount").textContent = "You have " + grandmas + " grandmas.";
+    document.getElementById("clicksPerSecond").textContent = "per second: " + clicksPerSecond;
+    document.getElementById("grandmaCount").textContent = grandmas;
     document.getElementById("grandmaPrice").textContent = grandmaPrice;
+    document.getElementById("farmCount").textContent = farmCount;
+    document.getElementById("farmPrice").textContent = farmPrice;
 }
 updateDisplay()
 function bake(bakeAmount) {
@@ -30,11 +34,19 @@ function buyGrandma(grandmaAmount) {
         grandmas+= grandmaAmount;
         cookies-= grandmaPrice;
     }
-    grandmaPrice = Math.ceil(100*(1.15**cursors));
+    grandmaPrice = Math.ceil(100*(1.15**grandmas));
+    updateDisplay();
+}
+function buyFarm(farmAmount) {
+    if (cookies >= farmPrice) {
+        farms+= farmAmount;
+        cookies-= farmPrice;
+    }
+    grandmaPrice = Math.ceil(100*(1.15**farms));
     updateDisplay();
 }
 setInterval(() => {
-    clicksPerSecond = ((cursors/10) + grandmas);
+    clicksPerSecond = ((cursors/10) + grandmas + farms);
     cookies+= clicksPerSecond;
     updateDisplay();
 }, 1000);
